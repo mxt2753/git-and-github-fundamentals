e66058c35928f3649ed56519bb7f4b46485e0a4f
Mary-Rose Tracy <111910818+mxt2753@users.noreply.github.com>
2023-10-21 14:05:42 -0500

    Delete git-and-github-fundamentals directory

--- git-and-github-fundamentals/hello.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main( int argc, char * argv[] )
-{
-  printf("My name is Mary-Rose Tracy");
-  printf("\nMy ID number is: 1001852753\n");
-  printf("Complete.\n");
-  return 0;
-}
